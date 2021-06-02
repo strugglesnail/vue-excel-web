9ed0f9e88ed9ddecc99a4ae7328036e28e4f6617
wtf <593616590@qq.com>
2021-06-02 19:48:23 +0800

    excel自动导入配置前端

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -12,9 +12,19 @@
 
       <!-- 获取sheet，分别展示对应的下拉框 -->
     <el-form label-position="right" label-width="80px">
-      <el-form-item v-for="item in sheetData" :label="item" :key="item">
-        <!--<el-input v-model="item"></el-input>-->
-        1111111111
+      <el-form-item v-for="(item, index) in sheetData" :label="item.sheetName" :key="item.sheetName">
+        <el-select v-model="value[index]" @change="((val)=>{change(val, item.sheetName)})" placeholder="请选择">
+          <el-option
+            v-for="t in item.tableList"
+            :key="t.label"
+            :label="t.label"
+            :value="t.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item v-if="sheetData && sheetData.length > 0">
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -27,7 +37,9 @@ export default {
   data() {
     return {
       fileList: [],
-      sheetData: []
+      sheetData: [],
+      value: [],
+      sheetParams: []
     }
   },
   created() {
@@ -36,22 +48,43 @@ export default {
   methods: {
     fetchData() {
     },
+    change(val, sheetName) {
+      console.log(val, sheetName)
+      if (this.sheetParams && this.sheetParams.length === 0) {
+
+      }
+      this.sheetParams.forEach(sheet => {
+        // 如果已经选择下拉框，则
+        if (sheet['sheetName'] === sheetName) {
+          sheet['fields'] = val
+        }
+      })
+    },
     handleChange(file, fileList) {
       if (file.response) {
-        this.sheetData = file.response.data
         getFields().then(res => {
           if (res.success) {
             const datas = []
             res.data.forEach(d => {
               datas.push({
-                label: ''
+                label: d.tableName + '(' + d.fieldNames + ')',
+                value: d.fieldList
               })
             })
-
+            file.response.data.forEach(sheetName => {
+              this.sheetData.push({
+                sheetName: sheetName,
+                tableList: datas
+              })
+            })
           }
         })
       }
       // this.fileList = fileList.slice(-3)
+    },
+    onSubmit() {
+      console.log(this.value)
+      console.log('submit!')
     }
   }
 }
